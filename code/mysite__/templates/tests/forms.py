591601a8458c15dc6bd9a47d59a48cf13836636f
jonathanlee <jonathan1214@foxmail.com>
2019-03-03 18:34:09 +0800

    sad

---
+++ code/mysite__/templates/tests/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
+
+
+class LoginForm(forms.Form):
+    attrs_for_stu_num = {'placeholder': '请输入学号'}
+    stu_num = forms.CharField(label='username', max_length=20, widget=forms.TextInput(attrs=attrs_for_stu_num))        # label是在页面上显示的
+    pwd = forms.CharField(label='password', initial='请输入密码', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        stu_num = cleaned_data.get('stu_num')
+
+        if not stu_num.isdigit():
+            msg = '学号请输入数字'
+            self.add_error('stu_num', msg)
+
+    # widget可以指定参数 如: widget = forms.TextInput(attrs={'class': 'specail'})
+    #                       这样就可以在视图中对这个以class='specail'进行渲染
+    # attrs:
+    #       包含要在呈现的窗口小部件上设置的HTML属性的字典。
+    # 1. 对html的属性十分清楚就可以在这里添加很多东西，这次我们添加了placeholder 1.14
+    #
+
+
+class SelectLabForm(forms.Form):
+    lab = forms.ChoiceField(label='选择实验室')
---
+++ code/mysite__/templates/tests/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .import views
+
+app_name = 'tests' # 命名空间！
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('your_name/', views.get_name, name='get_name'),
+    path('meta/', views.meta, name='meta'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('staff_profile/', views.stf_profile, name='stf_profile'),
+    path('res', views.my_res, name='my_res'),
+    path('lmake-resvervation/', views.make_reserversion_pre, name='m_r_p'),
+    path('make_res-t/', views.make_reserversion, name='m_r_t'),
+    path('change_pwd/', views.change_pwd, name='change_pwd'),
+]
+# 终于知道这个'name'有什么用了！
+# 故：在模板中使用：{% url 'app_name:name' param %}就可以映射到这个地方来！
+#可算是明白了！
